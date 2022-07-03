51512f74299a82ef14efb4f55c501505196e2c85
Prakhar <ptofficial29@gmail.com>
2022-07-04 00:45:58 +0530

    pwa added in about

--- src/components/About.js
+++ src/components/About.js
@@ -48,6 +48,15 @@ const About = () => {
             <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
             <path d="M22 4L12 14.01l-3-3"></path>
           </svg>
+          <span className="title-font font-medium text-white">Progressive Web App</span>
+        </div>
+      </div>
+      <div className="p-2 sm:w-1/2 w-full">
+        <div className="bg-gray-800 rounded flex p-4 h-full items-center">
+          <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="3" className="text-indigo-400 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
+            <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
+            <path d="M22 4L12 14.01l-3-3"></path>
+          </svg>
           <span className="title-font font-medium text-white">Google Search API</span>
         </div>
       </div>
